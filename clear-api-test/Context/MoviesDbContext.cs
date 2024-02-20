effd831c7bd62c52aedfd03557727e05aade341d
Francisco Escobar <jfescobar18@This-is-not-a-Hackingtosh-Pro.local>
2024-02-20 01:57:11 -0600

    add db context

---
+++ clear-api-test/Context/MoviesDbContext.cs
@@ -0,0 +1,39 @@
+﻿using DB;
+using Microsoft.EntityFrameworkCore;
+
+public class MoviesDbContext : DbContext
+{
+    public MoviesDbContext()
+    {
+    }
+
+    public MoviesDbContext(DbContextOptions<MoviesDbContext> options)
+        :base(options)
+    {
+
+    }
+
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Actor> Actors { get; set; }
+    public DbSet<MovieActor> MovieActors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json")
+               .Build();
+            var connectionString = configuration.GetConnectionString("MoviesConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Movie>().ToTable("Movie");
+        modelBuilder.Entity<Actor>().ToTable("Actors");
+        modelBuilder.Entity<MovieActor>().ToTable("MovieActor");
+    }
+}

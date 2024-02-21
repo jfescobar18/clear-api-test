1af6e905d7d9d1c5e3eca5ff0b265fe42c96045c
Francisco Escobar <jfescobar18@This-is-not-a-Hackingtosh-Pro.local>
2024-02-20 20:17:15 -0600

    fix controller

--- clear-api-test/Controllers/MoviesController.cs
+++ clear-api-test/Controllers/MoviesController.cs
@@ -2,99 +2,97 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace clear_api_test.Controllers
+[ApiController]
+[Route("api/[controller]")]
+public class MoviesController : ControllerBase
 {
-    [Route("api/[controller]")]
-    public class MoviesController : Controller
+    private readonly MoviesDbContext _context;
+
+    public MoviesController(MoviesDbContext context)
     {
-        private readonly MoviesDbContext _context;
+        _context = context;
+    }
 
-        public MoviesController(MoviesDbContext context)
+    private bool MovieExists(int id)
+    {
+        return _context.Movies.Any(e => e.MovieId == id);
+    }
+
+    // GET: api/Movies
+    [HttpGet]
+    public ActionResult<IEnumerable<Movie>> GetMovies()
+    {
+        return _context.Movies.ToList();
+    }
+
+    // GET: api/Movies/1
+    [HttpGet("{id}")]
+    public ActionResult<Movie> GetMovie(int id)
+    {
+        var movie = _context.Movies.Find(id);
+
+        if (movie == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        private bool MovieExists(int id)
+        return movie;
+    }
+
+    // POST: api/Movies
+    [HttpPost]
+    public ActionResult<Movie> PostMovie(Movie movie)
+    {
+        _context.Movies.Add(movie);
+        _context.SaveChanges();
+
+        return CreatedAtAction(nameof(GetMovie), new { id = movie.MovieId }, movie);
+    }
+
+    // PUT: api/Movies/1
+    [HttpPut("{id}")]
+    public IActionResult PutMovie(int id, Movie movie)
+    {
+        if (id != movie.MovieId)
         {
-            return _context.Movies.Any(e => e.MovieId == id);
+            return BadRequest();
         }
 
-        // GET: api/Movies
-        [HttpGet]
-        public ActionResult<IEnumerable<Movie>> GetMovies()
+        _context.Entry(movie).State = EntityState.Modified;
+
+        try
         {
-            return _context.Movies.ToList();
+            _context.SaveChanges();
         }
-
-        // GET: api/Movies/1
-        [HttpGet("{id}")]
-        public ActionResult<Movie> GetMovie(int id)
+        catch (DbUpdateConcurrencyException)
         {
-            var movie = _context.Movies.Find(id);
-
-            if (movie == null)
+            if (!MovieExists(id))
             {
                 return NotFound();
             }
-
-            return movie;
+            else
+            {
+                throw;
+            }
         }
 
-        // POST: api/Movies
-        [HttpPost]
-        public ActionResult<Movie> PostMovie(Movie movie)
+        return NoContent();
+    }
+
+    // DELETE: api/Movies/1
+    [HttpDelete("{id}")]
+    public IActionResult DeleteMovie(int id)
+    {
+        var movie = _context.Movies.Find(id);
+
+        if (movie == null)
         {
-            _context.Movies.Add(movie);
-            _context.SaveChanges();
-
-            return CreatedAtAction(nameof(GetMovie), new { id = movie.MovieId }, movie);
+            return NotFound();
         }
 
-        // PUT: api/Movies/1
-        [HttpPut("{id}")]
-        public IActionResult PutMovie(int id, Movie movie)
-        {
-            if (id != movie.MovieId)
-            {
-                return BadRequest();
-            }
+        _context.Movies.Remove(movie);
+        _context.SaveChanges();
 
-            _context.Entry(movie).State = EntityState.Modified;
-
-            try
-            {
-                _context.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MovieExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // DELETE: api/Movies/1
-        [HttpDelete("{id}")]
-        public IActionResult DeleteMovie(int id)
-        {
-            var movie = _context.Movies.Find(id);
-
-            if (movie == null)
-            {
-                return NotFound();
-            }
-
-            _context.Movies.Remove(movie);
-            _context.SaveChanges();
-
-            return NoContent();
-        }
+        return NoContent();
     }
 }
